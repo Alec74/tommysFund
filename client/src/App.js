bbd4ae1f34dee357f60c57d6ac4a7cf8edb89c07
Alec74 <alecmcglone@gmail.com>
2021-08-21 13:07:01 -0400

    navbar in place

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 // import SearchBooks from './pages/SearchBooks';
 // import SavedBooks from './pages/SavedBooks';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -17,8 +17,8 @@ function App() {
         <>
           <Navbar />
           <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
+            {/* <Route exact path='/' component={SearchBooks} /> */}
+            {/* <Route exact path='/saved' component={SavedBooks} /> */}
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
         </>
