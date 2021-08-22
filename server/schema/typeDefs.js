748cb99a440c6b6f4dc478d19774a55dfcaf988e
Alec74 <alecmcglone@gmail.com>
2021-08-22 11:32:53 -0400

    serverside user setup

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -3,12 +3,23 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 type User {
     _id: ID
-    username: String!
+    username: String
     email: String
+}
+
+type Auth {
+    token: ID
+    user: User
 }
 
 type Query {
     me: User
+}
+
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    login(email: String!, password: String!): Auth
 }
 `;
 
