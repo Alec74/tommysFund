98286dca3cf91b63c7d40d2d7db81b7818a5028a
Alec74 <alecmcglone@gmail.com>
2021-08-23 12:32:19 -0400

    cleanup

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -33,8 +33,8 @@ const ContactUs = () => {
 
     return (
         <div className={classes.root} >
-            <Grid container spacing={3}>
-                <Grid item xs={12} justifyContent="stretch" >
+            <Grid container spacing={3} justifyContent="center">
+                <Grid item xs={12}  >
                     <ContactForm></ContactForm>
                 </Grid>
             </Grid>
