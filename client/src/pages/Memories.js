a9402ff1c6af44ca69ded504f3a9d5d65ed6a9bc
Alec74 <alecmcglone@gmail.com>
2021-08-23 20:45:38 -0400

    2 songs play on memories page

--- client/src/pages/Memories.js
+++ client/src/pages/Memories.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Two from "..//images/IMG_1542.JPG";
+// import Demo from '../components/playButtons/Song1'
+import FirstSongCard from '../components/musicCards/FirstSongCard';
+import SecondSongCard from '../components/musicCards/SecondSongCard';
 
+import Grid from '@material-ui/core/Grid';
 
 const Memories = () => {
 
@@ -8,10 +12,15 @@ const Memories = () => {
 
     return (
         <>
-        <div>
-            <img src={Two}>
-            </img>
-        </div>
+            <div>
+                <br></br>
+                <Grid container spacing={10} justifyContent="center" direction="column" alignItems="center">
+                    <Grid item xs={12}>
+                        <FirstSongCard />
+                        <SecondSongCard />
+                    </Grid>
+                </Grid>
+            </div>
         </>
     )
 };
