93b0e8bd29534d02584942e1eb901fbce072b22a
Alec74 <alecmcglone@gmail.com>
2021-09-02 15:08:57 -0400

    added another song to memories and added intro and link to soundcloud

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,8 +3,8 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Dropdown from 'react-bootstrap/Dropdown'
-import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
 
 
 const useStyles = makeStyles((theme) => ({
--- client/src/pages/Memories.js
+++ client/src/pages/Memories.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Two from "..//images/IMG_1542.JPG";
 // import Demo from '../components/playButtons/Song1'
 import FirstSongCard from '../components/musicCards/FirstSongCard';
@@ -6,18 +9,48 @@ import SecondSongCard from '../components/musicCards/SecondSongCard';
 import ThirdSongCard from '../components/musicCards/ThirdSongCard';
 import FourthSongCard from '../components/musicCards/FourthSongCard';
 import FifthSongCard from '../components/musicCards/FifthSongCard';
-
+import SixthSongCard from '../components/musicCards/SixthSongCard';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from 'react-bootstrap/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'left',
+        color: theme.palette.text.secondary,
+    },
+    title: {
+        fontSize: 50
+    },
+    body: {
+        fontSize: 25
+    }
+}));
+
+{/* <Paper className={classes.paper}>
+                        <Typography className={classes.title} id="top" color="textSecondary">
+                            Welcome!
+                        </Typography>
+                    </Paper> */}
 
 const Memories = () => {
-
+    const classes = useStyles();
 
 
     return (
         <>
             <div>
                 <br></br>
+                <Typography className={classes.title} id="center" color="textSecondary">
+                    Music of Tom's
+                </Typography>
+                <br></br>
                 <Grid container spacing={3}>
+
                     <Grid item xs={6} sm={3}>
                         <FirstSongCard />
                     </Grid>
@@ -33,7 +66,13 @@ const Memories = () => {
                     <Grid item xs={6} sm={3}>
                         <FifthSongCard />
                     </Grid>
+                    <Grid item xs={6} sm={3}>
+                        <SixthSongCard />
+                    </Grid>
                 </Grid>
+                <br></br>
+                <Button variant="light"><a href="https://soundcloud.com/tmcg45" target="_blank">Link to his soundcloud</a></Button>
+                <br></br>
             </div>
         </>
     )
